15cf47947140773e43ca77c146987f20b6b14293
Michael <k1brusenets@gmail.com>
2021-11-25 20:28:08 +0200

    modalOk changed

--- src/components/modal/templatesModal/sendingDone.js
+++ src/components/modal/templatesModal/sendingDone.js
@@ -6,7 +6,7 @@ const SendingDone = ({children}) => {
     return (
         <div className='success'>
             <ShowImage img = './images/sendOk.svg' alt = 'sentOk'/>
-            <h2 className='success-title'>Дякуємо за Ваше питання!</h2>
+            <h3 className='success-title'>Дякуємо за Ваше питання!</h3>
             <p className='success-second-mess'>Найближчим часом ми Вам зателефонуємо!</p>
             <div className='success-btn'>{children}</div>
         </div>
