70eda926e9946c5a7df6e327d79c027444813a87
Michael <k1brusenets@gmail.com>
2021-11-27 16:20:33 +0200

    one char fault(

--- src/components/AboutUs/index.js
+++ src/components/AboutUs/index.js
@@ -12,7 +12,7 @@ const AboutUs = () => {
                 <p className = 'about-p'>Великий склад нових турбокомпресорів, б / у і після реставрації, а також комплектуючих до них (картриджі, актуатори, сервоприводу, ремкомплекти).</p>
             </div>
             <div>
-                <img src = './images/photoAbout.png' style = {{'width': '100%'}} alt = 'about us'/>
+                <img src = './images/photoabout.png' style = {{'width': '100%'}} alt = 'about us'/>
             </div>
             </div>
         </div>
