581d6f7bd0bc55a521cb85496db0b52abff248ef
Michael <k1brusenets@gmail.com>
2021-12-25 17:53:04 +0200

    added city and tel for mobile size

--- src/components/First/index.js
+++ src/components/First/index.js
@@ -12,7 +12,9 @@ const First = ({path}) => {
                 <h1 className = 'first-header'>
                     {/* <span className = 'first-span'>Ремонт</span>
                     та діагностика <span className = 'first-span-second'>турбін</span> */}
-                    ремонт та діагностика турбін будь-якої складності, на професійному обладнанні <span className = 'first-span'> всього за один день!</span>
+                    ремонт та діагностика турбін будь-якої складності, на професійному обладнанні<span className='first-city'>в м. Івано-Франківськ</span><a href = 'tel:+380962657550' className = 'first-link'>096 265-75-50</a>
+                <a href = 'tel:+380950970673' className = 'first-link'>095 097-06-73</a> <span className = 'first-span'> всього за один день!  </span>
+                  
                 </h1>
                 </div>
                 <Form path = {path}/>
